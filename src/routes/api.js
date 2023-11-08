903ded41b7db87d4c0e97f5b297d0f73121d646e
DucHuy2801 <theflash28012002@gmail.com>
2023-11-08 16:36:53 +0700

    add route postproject

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,10 +3,13 @@ const express = require("express")
 const router = express.Router()
 const { asyncHandler } = require('../auth/checkAuth')
 const accessController = require("../controllers/access.controller")
+const projectController = require("../controllers/project.controller")
 
 router.get('')
 router.post('/api/register', asyncHandler(accessController.register))
 router.post('/api/login', asyncHandler(accessController.login))
 router.post('/api/logout', asyncHandler(accessController.logout))
+
+router.post('/api/postproject', projectController.postProject)
 
 module.exports = router
