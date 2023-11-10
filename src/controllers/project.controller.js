0da61bb352750ea88889d1bf37b0f69a1a16e8ea
DucHuy2801 <theflash28012002@gmail.com>
2023-11-10 16:58:20 +0700

    update project api

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -3,6 +3,7 @@
 const ProjectService = require("../services/project.service")
 const {OK, CREATED, SuccessResponse} = require("../core/success.response")
 const Project = require("../models/project.model");
+const { BadRequestError } = require("../core/error.response");
 
 
 class ProjectController {
@@ -30,6 +31,33 @@ class ProjectController {
         metadata: await ProjectService.getAllProjects()
     }).send(res)
     };
+
+    updateProject = async (req, res) => {
+      const project_id = req.params.project_id;
+      const foundProject = Project.findOne({where: {project_id}})
+      if (!foundProject) {
+        throw new BadRequestError('Not found project for updating!')
+      }
+      console.log(`body`, req.body)
+      Project.update(req.body, {
+        where: {project_id: project_id}
+      }).then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Project was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update Project with project_id=${project_id}. Maybe Project was not found or req.body is empty!`
+          });
+        }
+        })
+        .catch(err => {
+          res.status(500).send({
+            message: "Error updating project with project_id=" + project_id
+          });
+        })
+    }
 
     deleteProjectById = async (req, res) => {
         const projectId = req.params.project_id;
