15c336146272586f2f28fab3b0022fb8deebed15
DucHuy2801 <theflash28012002@gmail.com>
2023-11-11 01:06:32 +0700

    update

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -36,12 +36,44 @@ const User = sequelize.define('user', {
     },
     mssv: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: true
     },
     school: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: true
     }
 })
+
+// Admin
+// User.create({
+//     username: "admin",
+//     password: "123456",
+//     fullname: "Nguyen Duc Huy",
+//     gmail: "huy.nguyen28012002@hcmut.edu.vn",
+//     type_user: accountTypes.ADMIN,
+//     mssv: null,
+//     school: "Dai hoc Bach Khoa HCM"
+// })
+
+// // Community Leaders
+// User.create({
+//     username: "leader1",
+//     password: "123456",
+//     fullname: "Leader 1",
+//     gmail: "leader1@gmail.com",
+//     type_user: accountTypes.LEADER,
+//     mssv: null,
+//     school: null
+// })
+
+// User.create({
+//     username: "leader2",
+//     password: "456789",
+//     fullname: "Leader 2",
+//     gmail: "leader2@gmail.com",
+//     type_user: accountTypes.LEADER,
+//     mssv: null,
+//     school: null
+// })
 
 module.exports = User
