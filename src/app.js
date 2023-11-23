0e5e5813254dd345b0d947a47f41ea54163e003d
DucHuy2801 <theflash28012002@gmail.com>
2023-11-23 15:31:22 +0700

    fix init route

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(helmet())
 app.use(compression())
 app.use(express.json()) 
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }))
 app.use(bodyParser.json())
 
@@ -34,17 +34,17 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 
 // init routes
 app.get("/", (req, res) => {
-  res.json({ message: "Backend for Mua he xanh Online..." });
+    res.json({ message: "Backend for Mua he xanh Online..." });
 });
-app.use('', require('./routes/api'))
+app.use('', require('./routes'))
 
 app.use((error , req, res, next) => {
     const statusCode = error.status || 500
     return res.status(statusCode).json({
-      status: 'error',
-      code: statusCode,
-      message: error.message || 'Internal Server Error'
+        status: 'error',
+        code: statusCode,
+        message: error.message || 'Internal Server Error'
     })
-  })
+})
   
 module.exports = app
