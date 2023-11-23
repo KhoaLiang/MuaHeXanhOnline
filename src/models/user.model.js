8599a286e2e8cc7eba34ae2e48d8be16854a3d03
DucHuy2801 <theflash28012002@gmail.com>
2023-11-24 00:23:34 +0700

    fix user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -41,6 +41,14 @@ const User = sequelize.define('user', {
     school: {
         type: DataTypes.STRING,
         allowNull: true
+    },
+    reset_password_token: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    reset_password_expires: {
+        type: DataTypes.DATE,
+        allowNull: true
     }
 })
 
