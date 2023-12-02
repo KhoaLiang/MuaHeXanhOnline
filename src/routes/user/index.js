60a92fa01d86ba289bc1b08c8ed69b0a6ce69c44
DucHuy2801 <theflash28012002@gmail.com>
2023-12-02 12:09:28 +0700

    fix route user

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -5,7 +5,7 @@ const userController = require("../../controllers/user.controller")
 const router = express.Router()
 const { asyncHandler } = require("../../helpers/asyncHandler")
 
-router.get('/:mssv', asyncHandler(userController.getStudentByMSSV))
 router.get('/all', asyncHandler(userController.getAllStudents))
+router.get('/:mssv', asyncHandler(userController.getStudentByMSSV))
 router.put('/:mssv', asyncHandler(userController.updateInfoStudent))
 module.exports = router
