b12cbd0ebb286f5a49e29f0b0dbbc42a241aef7f
DucHuy2801 <theflash28012002@gmail.com>
2023-12-02 12:08:41 +0700

    fix route

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,7 @@
-''
+'use strict'
 const express = require("express")
 const router = express.Router()
-const { asyncHandler } = require('../auth/checkAuth')
+const { asyncHandler } = require("../helpers/asyncHandler")
 const accessController = require("../controllers/access.controller")
 const projectController = require("../controllers/project.controller")
 const userController = require("../controllers/user.controller")
@@ -10,17 +10,21 @@ router.get('')
 router.post('/v1/api/user/register', asyncHandler(accessController.register))
 router.post('/v1/api/user/login', asyncHandler(accessController.login))
 // router.post('/v1/api/user/logout', asyncHandler(accessController.logout))
-router.post('/v1/api/user/forgotpassword', asyncHandler(userController.forgotPassword))
+
+router.post('/v1/api/user/forgotpassword', asyncHandler(accessController.forgotPassword))
+router.post('/v1/api/user/resetpassword', asyncHandler(accessController.resetPassword))
 
 router.post('/v1/api/project', asyncHandler(projectController.postProject))
 router.get('/v1/api/project/all', asyncHandler(projectController.getAllProjects))
-router.put('/v1/api/project/:project_id', asyncHandler(projectController.updateProject))
+router.get('/v1/api/project/:project_id', asyncHandler(projectController.getProjectById))
+router.patch('/v1/api/project/:project_id', asyncHandler(projectController.updateProject))
 router.delete('/v1/api/project/:project_id', projectController.deleteProjectById)
-router.put('/v1/project/verify/:project_id', asyncHandler(projectController.verifyProject))
-router.put('/v1/project/verified_projects', asyncHandler(projectController.verifyProject))
 
-router.get('/v1/api/student/:mssv', asyncHandler(userController.getStudentByMSSV))
+router.patch('/v1/api/project/verify/:project_id', asyncHandler(projectController.verifyProject))
+router.put('/v1/api/project/verified_projects', asyncHandler(projectController.getAllVerifiedProjects))
+
 router.get('/v1/api/student/all', asyncHandler(userController.getAllStudents))
+router.get('/v1/api/student/:mssv', asyncHandler(userController.getStudentByMSSV))
 router.put('/v1/api/student/:mssv', asyncHandler(userController.updateInfoStudent))
 
 module.exports = router
