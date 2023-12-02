3b6e6149fa3587aecc5f582cfb98efd8d297a1c0
DucHuy2801 <theflash28012002@gmail.com>
2023-12-02 12:08:22 +0700

    application service

---
+++ src/services/application.service.js
@@ -0,0 +1,115 @@
+'use strict'
+
+const { NotFoundError } = require("../core/error.response");
+const Application = require("../models/application.model");
+const Project = require("../models/project.model");
+const User = require("../models/user.model");
+
+class ApplicationService {
+    static createApplication = async ({mssv, project_id}) => {
+        const newApplication = new Application({mssv_student: mssv, project_id: project_id, status: false})
+        const savedApplication = await newApplication.save().catch((error) => {
+            console.log("Error", error);
+            return new BadRequestError("Error: Cannot create application at the moment");
+        });
+        if (savedApplication) {
+            return {
+                code: 201,
+                message_data: savedApplication,
+            };
+          }
+        return {
+            code: 200,
+            metadata: null,
+        };
+    }
+
+    static getApplicationById = async (apply_id) => {
+        try {
+            const apply = await Application.findOne({ where: { apply_id } });
+      
+            if (apply) {
+                return {
+                    success: true,
+                    data: apply,
+                };
+            } else {
+                return {
+                    success: false,
+                    error: "Application not found",
+                };
+            }
+          } catch (error) {
+                console.error("Failed to retrieve application data: ", error);
+                return {
+                    success: false,
+                    error: "An error occurred",
+                };
+          }
+    }
+
+    static getAllApplication = async () => {
+        try {
+            const applies = await Application.findAll();
+            return {
+                success: true,
+                data: applies,
+            };
+        } catch (error) {
+            console.error("Failed to retrieve applications data: ", error);
+            return {
+                success: false,
+                error: "An error occurred",
+            };
+        }
+    };
+
+    static approveApplication = async ({apply_id, status}) => {
+        const foundApplication = await Application.findOne({ where: {apply_id: apply_id}})
+        if (!foundApplication) throw new NotFoundError("Not found Application")
+
+        const [num, updatedRows] = await Application.update({status: status}, {
+            where: { apply_id: apply_id },
+        });
+
+        if (num === 1) {
+            return {
+                success: true,
+                message: "Approve application successfully!"
+            }
+        } else {
+            return {
+                success: false,
+                data: 'Fail!'
+            }
+        }
+    }
+
+    static deleteApplicationById = async (applicationId) => {
+        try {
+            const application = await Application.findOne({
+                where: { apply_id: applicationId },
+            });
+            if (!application) {
+                return {
+                success: false,
+                error: "Application not found",
+                };
+            }
+            await application.destroy();
+    
+            return {
+                success: true,
+                message: "Application deleted successfully",
+            };
+        } catch (error) {
+            console.error("Failed to delete application: ", error);
+            return {
+                success: false,
+                error: "An error occured",
+            };
+        }
+    };
+}
+
+module.exports = ApplicationService
