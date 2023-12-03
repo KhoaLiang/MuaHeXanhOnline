7a6cd6b45fc4b6c9802d064c4f6a6dda641b2611
DucHuy2801 <theflash28012002@gmail.com>
2023-12-03 15:32:54 +0700

    add router application

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,8 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/v1/api', require("./access"))
-router.use('/v1/api/product', require("./project"))
-router.use('/v1/api/user', require("./user"))
+router.use('/v1/api/project', require("./project"))
+router.use('/v1/api/student', require("./user"))
+router.use('/v1/api/application', require("./application"))
 
 module.exports = router
