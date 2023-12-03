b1a6847ed76cf4dbf87ba8f76ad573327a9f943c
DucHuy2801 <theflash28012002@gmail.com>
2023-12-03 15:33:33 +0700

    authentication admin

--- src/routes/project/index.js
+++ src/routes/project/index.js
@@ -4,12 +4,16 @@ const express = require("express")
 const projectController = require("../../controllers/project.controller")
 const router = express.Router()
 const { asyncHandler } = require("../../helpers/asyncHandler")
+const { authenticationAdmin } = require("../../auth/authUtils")
 
 router.post('', asyncHandler(projectController.postProject))
 router.get('/all', asyncHandler(projectController.getAllProjects))
 router.get('/:project_id', asyncHandler(projectController.getProjectById))
 router.patch('/:project_id', asyncHandler(projectController.updateProject))
 router.delete('/:project_id', projectController.deleteProjectById)
+
+router.use(authenticationAdmin)  
+
 router.patch('/verify/:project_id', asyncHandler(projectController.verifyProject))
 // router.put('/verified_projects', asyncHandler(projectController.verifyProject))
 
