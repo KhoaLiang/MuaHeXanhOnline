fb1100298270341daff111b4acca8b692359f2af
DucHuy2801 <theflash28012002@gmail.com>
2023-12-03 19:28:02 +0700

    add forgot & reset password route

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -8,4 +8,7 @@ const { asyncHandler } = require("../../helpers/asyncHandler")
 router.post('/user/register', asyncHandler(accessController.register))
 router.post('/user/login', asyncHandler(accessController.login))
 
+router.post('/user/forgotpassword', asyncHandler(accessController.forgotPassword))
+router.post('/user/resetpassword', asyncHandler(accessController.resetPassword))
+
 module.exports = router
