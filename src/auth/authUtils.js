1d71bd0906136586d9c64152f6027ba4c06a3361
DucHuy2801 <theflash28012002@gmail.com>
2023-12-03 15:32:28 +0700

    authentication leader & admin

--- src/auth/authUtils.js
+++ src/auth/authUtils.js
@@ -2,6 +2,10 @@
 
 const JWT = require('jsonwebtoken')
 const { AuthFailureError, NotFoundError } = require('../core/error.response')
+const { asyncHandler } = require('../helpers/asyncHandler')
+const HEADER = {
+    AUTHORIZATION: 'authorization' 
+}
 
 const createTokenPair = async (payload, publicKey, privateKey) => {
     try {
@@ -30,7 +34,39 @@ const verifyJWT = async (token, keySecret) => {
     return await JWT.verify(token, keySecret)
 }
 
+const authenticationAdmin = asyncHandler(async (req, res, next) => {
+    const token = req.headers[HEADER.AUTHORIZATION]
+    if (!token) throw new AuthFailureError('Invalid Request')
+
+    try {
+        const decodeUser = JWT.decode(token)
+        console.log(`decodeUser`, decodeUser)
+        if (decodeUser.type_user !== 'admin') throw new AuthFailureError('Unauthorized')
+        req.user = decodeUser
+        return next()
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+const authenticationLeader = asyncHandler(async (req, res, next) => {
+    const token = req.headers[HEADER.AUTHORIZATION]
+    if (!token) throw new AuthFailureError('Invalid Request')
+
+    try {
+        const decodeUser = JWT.decode(token)
+        console.log(`decodeUser`, decodeUser)
+        if (decodeUser.type_user !== 'leader') throw new AuthFailureError('Unauthorized')
+        req.user = decodeUser
+        return next()
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 module.exports = {
     createTokenPair,
-    verifyJWT
+    verifyJWT,
+    authenticationAdmin,
+    authenticationLeader
 }
