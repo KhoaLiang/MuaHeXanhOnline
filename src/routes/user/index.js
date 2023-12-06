14a24eef395433d1c15e3175424f4bbe92bcb205
DucHuy2801 <theflash28012002@gmail.com>
2023-12-06 22:09:06 +0700

    authen student

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -4,8 +4,10 @@ const express = require("express")
 const userController = require("../../controllers/user.controller")
 const router = express.Router()
 const { asyncHandler } = require("../../helpers/asyncHandler")
+const { authenticationStudent } = require("../../auth/authUtils")
 
 router.get('/all', asyncHandler(userController.getAllStudents))
+router.use(authenticationStudent)
 router.get('/:mssv', asyncHandler(userController.getStudentByMSSV))
 router.put('/:mssv', asyncHandler(userController.updateInfoStudent))
 module.exports = router
